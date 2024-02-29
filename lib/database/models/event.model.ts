690b5bcfe47c311254b0aab96e86e981eb12168e
Aadesh098 <48157409+Aadesh098@users.noreply.github.com>
2024-02-29 14:41:28 +0530

    Final Edits

--- lib/database/models/event.model.ts
+++ lib/database/models/event.model.ts
@@ -21,13 +21,13 @@ const EventSchema = new Schema({
   description: { type: String },
   location: { type: String },
   createdAt: { type: Date, default: Date.now },
-  imageUrl: { type: String, required: true },
+  imageUrl: { type: String},
   startDateTime: { type: Date, default: Date.now },
   endDateTime: { type: Date, default: Date.now },
   price: { type: String },
   isFree: { type: Boolean, default: false },
   url: { type: String },
-  category: { type: Schema.Types.ObjectId, ref: 'Category' },
+  category: { type: Schema.Types.ObjectId, ref: 'Category' , default: 'Finance' },
   organizer: { type: Schema.Types.ObjectId, ref: 'User' },
 })
 
