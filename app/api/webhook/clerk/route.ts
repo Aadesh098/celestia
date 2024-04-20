e775ebcd7dff613b209c7c4c364ddb68e39e3ae4
Aadesh098 <48157409+Aadesh098@users.noreply.github.com>
2024-04-20 16:18:21 +0530

    Ready For Deployment

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -22,7 +22,7 @@ export async function POST(req: Request) {
  
   // If there are no headers, error out
   if (!svix_id || !svix_timestamp || !svix_signature) {
-    return new Response('Error occured -- no svix headers', {
+    return new Response('Error occurred -- no svix headers', {
       status: 400
     })
   }
@@ -45,7 +45,7 @@ export async function POST(req: Request) {
     }) as WebhookEvent
   } catch (err) {
     console.error('Error verifying webhook:', err);
-    return new Response('Error occured', {
+    return new Response('Error occurred', {
       status: 400
     })
   }
@@ -61,9 +61,9 @@ export async function POST(req: Request) {
       clerkId: id,
       email: email_addresses[0].email_address,
       username: username!,
-      firstName: first_name,
-      lastName: last_name,
-      photo: image_url,
+      firstName: first_name || '',
+      lastName: last_name || '',
+      photo: image_url || '',
     }
 
     const newUser = await createUser(user);
@@ -83,10 +83,10 @@ export async function POST(req: Request) {
     const {id, image_url, first_name, last_name, username } = evt.data
 
     const user = {
-      firstName: first_name,
-      lastName: last_name,
+      firstName: first_name || '',
+      lastName: last_name || '',
       username: username!,
-      photo: image_url,
+      photo: image_url || '',
     }
 
     const updatedUser = await updateUser(id, user)
@@ -104,4 +104,3 @@ export async function POST(req: Request) {
  
   return new Response('', { status: 200 })
 }
- 
